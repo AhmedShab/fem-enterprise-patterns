0262527afed8cc80fd1fef8f06c19bd99638c292
AhmedShab <hello@ahmedshaaban.tech>
2021-03-24 09:03:06 +1300

    solve first challenge

--- apps/micro/src/app/home/home.component.ts
+++ apps/micro/src/app/home/home.component.ts
@@ -12,23 +12,50 @@ export class HomeComponent {
   mode;
   widgets: Widget[];
 
-  updateWidgetsAndRecalculateCost(widget: Widget) {
-    switch (this.mode) {
+  reCalculateTotal(mode: string, widgets: Array<Widget>, widget: Widget) {
+    switch (mode) {
       case 'create':
-        const newWidget = Object.assign({}, widget, { id: uuidv4() });
-        this.widgets = [...this.widgets, newWidget];
+        this.updateWidgets(mode, widgets, widget);
+        this.getTotalPrice(widgets);
         break;
       case 'update':
-        this.widgets = this.widgets.map(_widget => widget.id === _widget.id
-          ? Object.assign({}, widget) : _widget);
+        this.getTotalPrice(widgets);
         break;
       case 'delete':
-        this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
+        this.getTotalPrice(widgets);
         break;
       default:
         break;
-    }
-    return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
+    } 
   }
 
+  updateWidgets(mode: string, widgets: Array<Widget>, widget: Widget) {
+    switch (mode) {
+      case 'create':
+        return this.addWidget(widgets, widget);
+      case 'update':
+        return this.updateWidget(widgets, widget);
+      case 'delete':
+        return this.deleteWidget(widgets, widget);
+      default:
+        break;
+    }
+  }
+
+  addWidget(widgets: Array<Widget>, widget: object) {
+    const newWidget = Object.assign({}, widget, { id: uuidv4() });
+    return [...widgets, newWidget];
+  }
+
+  updateWidget(widgets: Array<any>, widget: any) {
+    return widgets.map(_widget => widget.id === _widget.id
+      ? Object.assign({}, widget) : _widget);
+  }
+  deleteWidget(widgets: Array<any>, widget: any) {
+    return widgets.filter(_widget => widget.id !== _widget.id);
+  }
+
+  getTotalPrice(widgets) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
 }
